55a4f6abe42f6dbd0880450bb804a89d4124ba38
Meli Imelda <meliimeldatchouala@gmail.com>
2023-04-10 21:27:44 +0100

    ok

---
+++ main.py
@@ -0,0 +1,9 @@
+# This entrypoint file to be used in development. 
+import demographic_data_analyzer
+from unittest import main
+
+# Test your function by calling it here
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run unit tests automatically
+#main(module='test_module', exit=False)
