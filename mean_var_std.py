f12d154378ed6e1bc21ac457be8e09d07db725a7
The-analyst001 <124140521+The-analyst001@users.noreply.github.com>
2023-04-11 09:16:30 +0100

    Update mean_var_std.py
    This code is a good implementation of : slicing , list comprehension , and selection with the numpy package.

--- mean_var_std.py
+++ mean_var_std.py
@@ -17,4 +17,6 @@ def calculate(list):
 'sum': [[d[:,i].sum() for i in range(3)], [d[i].sum() for i in range (3) ] , d.sum()]
   }
     return dic
+  #Example with the list [0,1,2,3,4,5,6,7,8]
+calculate([0,1,2,3,4,5,6,7,8])
 
